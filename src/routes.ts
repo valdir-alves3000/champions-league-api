80c6e8a764e3221278a185c4b305f680eb59fafe
valdir-alves3000 <valdiralves3000@gmail.com>
2024-06-03 09:07:38 -0300

    feat: add routes

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getPlayer, getPlayerById, postPlayer } from "./controllers/players-controller";
+
+const router = Router()
+
+router.get("/players", getPlayer)
+router.get("/players/:id", getPlayerById)
+
+router.post("/players", postPlayer)
+
+export { router };
