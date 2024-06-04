77cda3a5f17252cc933c4c82cf59dd110d51dcfc
valdir-alves3000 <valdiralves3000@gmail.com>
2024-06-04 11:00:34 -0300

    chore: add config cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+import cors from 'cors'
 import express from 'express'
 import { router } from './routes'
 
 const app = express()
 
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PATCH", "DELETE"]
+}
+
 app.use(express.json())
+app.use(cors(corsOptions))
 app.use("/api", router)
 
 export { app }
